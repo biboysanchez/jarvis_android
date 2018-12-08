e7731849fe5ffeb7b66c5fde29aafbb39fe75448
pardz <laurencepardz11@gmail.com>
2018-12-08 20:34:44 +0800

    added multiple line chart

--- app/src/main/java/com/jarvis/app/fragment/TimeSeriesFragment.kt
+++ app/src/main/java/com/jarvis/app/fragment/TimeSeriesFragment.kt
@@ -2,25 +2,22 @@ package com.jarvis.app.fragment
 
 import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import com.github.mikephil.charting.components.Description
-import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.github.mikephil.charting.utils.EntryXComparator
 import com.jarvis.app.R
 import com.jarvis.app.adapter.HomeListAdapter
 import com.jarvis.app.utils.Util
 import kotlinx.android.synthetic.main.fragment_time_series.*
 import java.util.*
-import com.github.mikephil.charting.components.YAxis
-
-
 
 class TimeSeriesFragment : BaseFragment() {
     override fun setTitle(): String {
@@ -41,6 +38,7 @@ class TimeSeriesFragment : BaseFragment() {
         setRecyclerAdapter()
         setSpinner()
         returnLineChart()
+        returnPortfolioListChart()
     }
 
     private fun setSpinner(){
@@ -105,5 +103,72 @@ class TimeSeriesFragment : BaseFragment() {
         val data = LineData(dataSets)
         lineChartReturnBenchMark?.data = data
         lineChartReturnBenchMark?.invalidate()
+    }
+
+    private fun returnPortfolioListChart(){
+        val entries = ArrayList<Entry>()
+        entries.add(Entry(10f, 80f))
+        entries.add(Entry(20f, 85f))
+        entries.add(Entry(30f, 80f))
+        entries.add(Entry(40f, 86f))
+        entries.add(Entry(50f, 81f))
+        entries.add(Entry(60f, 87f))
+
+        val entries2 = ArrayList<Entry>()
+        entries2.add(Entry(10f, 75f))
+        entries2.add(Entry(20f, 74f))
+        entries2.add(Entry(30f, 72f))
+        entries2.add(Entry(40f, 78f))
+        entries2.add(Entry(50f, 74f))
+        entries2.add(Entry(60f, 78f))
+
+        val entries3 = ArrayList<Entry>()
+        entries3.add(Entry(10f, 65f))
+        entries3.add(Entry(20f, 66f))
+        entries3.add(Entry(30f, 62f))
+        entries3.add(Entry(40f, 64f))
+        entries3.add(Entry(50f, 58f))
+        entries3.add(Entry(60f, 67f))
+
+        // sort by x-value
+        Collections.sort(entries, EntryXComparator())
+
+        // create a dataset and give it a type
+        val set1 = LineDataSet(entries, "DataSet 1")
+        set1.lineWidth = 0f
+        set1.setDrawCircles(false)
+        set1.setDrawFilled(true)
+        set1.valueTextColor = android.R.color.transparent
+        set1.fillDrawable = ContextCompat.getDrawable(context!!, R.drawable.solid_green_lvl1)
+
+        // create a dataset and give it a type
+        val set2 = LineDataSet(entries2, "DataSet 2")
+        set2.lineWidth = 0f
+        set2.setDrawCircles(false)
+        set2.setDrawFilled(true)
+        set2.valueTextColor = android.R.color.transparent
+        set2.fillDrawable = ContextCompat.getDrawable(context!!, R.drawable.solid_green_lvl2)
+
+        // create a dataset and give it a type
+        val set3 = LineDataSet(entries3, "DataSet 3")
+        set3.lineWidth = 0f
+        set3.setDrawCircles(false)
+        set3.setDrawFilled(true)
+        set3.valueTextColor = android.R.color.transparent
+        set3.fillDrawable = ContextCompat.getDrawable(context!!, R.drawable.solid_green_lvl3)
+
+        // create a data object with the data sets
+        val data = LineData(set1, set2, set3)
+
+        portfolioLineChart?.legend?.isEnabled = false
+        portfolioLineChart?.description = null
+        portfolioLineChart?.axisRight?.setDrawLabels(false)
+        portfolioLineChart?.setDrawGridBackground(true)
+        portfolioLineChart?.setGridBackgroundColor(Color.parseColor("#2C7B74"))
+
+
+        // set data
+        portfolioLineChart?.data = data
+        portfolioLineChart?.invalidate()
     }
 }
--- app/src/main/java/com/jarvis/app/https/API.kt
+++ app/src/main/java/com/jarvis/app/https/API.kt
@@ -1,6 +1,6 @@
 package com.jarvis.app.https
 
 object API {
-    private const val baseUrl = ""
+    private const val baseUrl = "http://jarvis-mobile-api.blipcom.com"
     const val login             = "$baseUrl/login"
 }
