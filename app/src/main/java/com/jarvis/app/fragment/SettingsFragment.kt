347cd486183e286667660e82eb3475a7153d9405
pardz <laurencepardz11@gmail.com>
2018-12-31 22:40:01 +0800

    added pin dialog

--- app/src/main/java/com/jarvis/app/fragment/SettingsFragment.kt
+++ app/src/main/java/com/jarvis/app/fragment/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package com.jarvis.app.fragment
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.jarvis.app.R
+import kotlinx.android.synthetic.main.dialog_pin.view.*
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment: BaseFragment() {
     override fun setTitle(): String {
@@ -22,6 +25,46 @@ class SettingsFragment: BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         configToolBar(true)
+        onCheckedListener()
+    }
+
+    private fun onCheckedListener(){
+        switchCompat?.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked){
+                showPinDialog()
+            }
+        }
+    }
+
+    private fun showPinDialog(){
+        val alert = AlertDialog.Builder(context!!)
+        val aView = LayoutInflater.from(context!!).inflate(R.layout.dialog_pin, null)
+        alert.setView(aView)
+
+        val dialog = alert.create()
+        aView.btnCancelPin?.setOnClickListener {
+            switchCompat?.isChecked = false
+            dialog.dismiss()
+        }
+
+        aView.tvNextPin?.setOnClickListener {
+            if (aView.etDialogPin?.text.toString().isEmpty() || aView.etDialogPin?.text?.length!! < 6){
+                aView.etDialogPin?.error = "Invalid pin"
+                return@setOnClickListener
+            }
+            aView.etDialogPin?.error = null
+
+            if (aView.etDialogPin?.text.toString() != aView.etDialogVerifyPin?.text.toString()){
+                aView.etDialogVerifyPin?.error = "Pin not match"
+                return@setOnClickListener
+            }
+
+            aView.etDialogVerifyPin?.error = null
+            dialog.dismiss()
+        }
+
+        dialog.setCancelable(false)
+        dialog.show()
     }
 
     override fun onDestroyView() {
